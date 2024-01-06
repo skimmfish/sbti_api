23f941fb2b430e46bb8efe82ddfbba410b72c335
Olabiyi Ayodeji <89249310+skimmfish@users.noreply.github.com>
2024-01-06 14:09:21 +0100

    06-01-2023
    Addition of react front end to the mix

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         react(),
