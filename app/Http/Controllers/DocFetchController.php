1da2405dfcc76e108316429f05cf4c48907a7372
Olabiyi Ayodeji <89249310+skimmfish@users.noreply.github.com>
2024-01-12 14:55:38 +0100

    composer.lock updated
    composer.lock updated

--- app/Http/Controllers/DocFetchController.php
+++ app/Http/Controllers/DocFetchController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Excel;
+//use Excel;
 
 class DocFetchController extends Controller
 {
@@ -53,8 +53,8 @@ class DocFetchController extends Controller
          // Get the uploaded file
          $file = $request->file('import_f');
 
-          // ProcessING the Excel file 
-         Excel::import(new \App\Imports\ImportClass, $file);
+          // ProcessING the Excel file
+        // Excel::import(new \App\Imports\ImportClass, $file);
 
          //RETURNING RESPONSE TO THE FRONTEND
          return response()->json(['message'=>'Doc. data imported successfully','status'=>true],200);
